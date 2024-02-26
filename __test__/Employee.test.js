b2dac9a29326c65da4bdeb373a7d6e8e84b02424
CNtervisi <c.ntervisi@outlook.com>
2024-02-26 22:21:47 +0500

    started working on the tests second fix

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require("../lib/Employee.js");
+import Employee from "../lib/Employee.js";
 
 test("creates an employee object", () => {
   const employee = new Employee("CNtervisi", 12, "c.ntervisi@outlook.com");
